bf9004c3e832f36a64dede0e32adc4a95e16678f
monsterooo <createthink.net@gmail.com>
2020-02-27 18:35:05 +0800

    源码阅读

--- debug/dist/main.js
+++ debug/dist/main.js
@@ -1,114 +1,15 @@
 /******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
-
-/***/ "./node_modules/object.is/index.js":
-/*!*****************************************!*\
-  !*** ./node_modules/object.is/index.js ***!
-  \*****************************************/
-/*! unknown exports (runtime-defined) */
-/*! export default [maybe provided (runtime-defined)] [used] [usage and provision prevents renaming] */
-/*! other exports [maybe provided (runtime-defined)] [maybe used (runtime-defined)] */
-/*! runtime requirements: module, __webpack_require__ */
-/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
-
-module.exports = typeof Object.is === 'function' ? Object.is : __webpack_require__(/*! ./is */ "./node_modules/object.is/is.js")
-
-
-/***/ }),
-
-/***/ "./node_modules/object.is/is.js":
-/*!**************************************!*\
-  !*** ./node_modules/object.is/is.js ***!
-  \**************************************/
-/*! unknown exports (runtime-defined) */
-/*! exports [maybe provided (runtime-defined)] [maybe used (runtime-defined)] */
-/*! runtime requirements: module */
-/***/ ((module) => {
-
-var is = function (x, y) {
-  if (x === y) {
-    return x !== 0 || 1 / x === 1 / y
-  } else {
-    return x !== x && y !== y
-  }
-}
-
-module.exports = is
-
-
-/***/ })
-
-/******/ 	});
-/************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		if(__webpack_module_cache__[moduleId]) {
-/******/ 			return __webpack_module_cache__[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/compat get default export */
-/******/ 	(() => {
-/******/ 		// getDefaultExport function for compatibility with non-harmony modules
-/******/ 		__webpack_require__.n = (module) => {
-/******/ 			var getter = module && module.__esModule ?
-/******/ 				() => module['default'] :
-/******/ 				() => module;
-/******/ 			__webpack_require__.d(getter, { a: getter });
-/******/ 			return getter;
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__webpack_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop)
-/******/ 	})();
-/******/ 	
-/************************************************************************/
-(() => {
-"use strict";
+/******/ 	"use strict";
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
 /*! namespace exports */
 /*! exports [not provided] [unused] */
-/*! runtime requirements: __webpack_require__, __webpack_require__.n, __webpack_require__.* */
-/* harmony import */ var object_is__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! object.is */ "./node_modules/object.is/index.js");
-/* harmony import */ var object_is__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(object_is__WEBPACK_IMPORTED_MODULE_0__);
+/*! runtime requirements:  */
+!(function webpackMissingModule() { var e = new Error("Cannot find module 'object.is'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
 
 console.log('很高兴认识你，webpack');
-console.log(object_is__WEBPACK_IMPORTED_MODULE_0___default()(1, 1));
-})();
-
+console.log(!(function webpackMissingModule() { var e = new Error("Cannot find module 'object.is'"); e.code = 'MODULE_NOT_FOUND'; throw e; }())(1, 1));
 /******/ })()
 ;
 //# sourceMappingURL=main.js.map
